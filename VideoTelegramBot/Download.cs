7528cd1ee2096f1c8e359963db9ffd9e77b61a29
Давид <manvelyancda@masterscada.ru>
2024-12-15 15:51:28 +0300

    fix warnings

--- VideoTelegramBot/Download.cs
+++ VideoTelegramBot/Download.cs
@@ -7,7 +7,7 @@ public class Download
 {
     public string? FilePath { get; set; }
     public string? FileName => Path.GetFileName(FilePath);
-    public IVideo Video { get; set; }
-    public VideoDownloadPreference VideoDownloadPreference { get; set; }
-    public VideoDownloadOption VideoDownloadOption { get; set; }
+    public IVideo Video { get; set; } = default!;
+    public VideoDownloadPreference VideoDownloadPreference { get; set; } = default!;
+    public VideoDownloadOption VideoDownloadOption { get; set; } = default!;
 }
