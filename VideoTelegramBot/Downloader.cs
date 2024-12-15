36991f0496c1f702e50f4394e5aef19ac1341afd
Давид <manvelyancda@masterscada.ru>
2024-12-15 22:18:18 +0300

    update lib

--- VideoTelegramBot/Downloader.cs
+++ VideoTelegramBot/Downloader.cs
@@ -38,6 +38,7 @@ public class Downloader
             download.VideoDownloadOption = await downloader.GetBestDownloadOptionAsync(
                     download.Video!.Id,
                     download.VideoDownloadPreference,
+                    false,
                     cancellationToken
                 );
 
