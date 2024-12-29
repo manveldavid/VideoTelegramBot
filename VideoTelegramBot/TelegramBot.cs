a37e693fa8b6b96f73769fcd5384f6736ad79ccf
Давид <manvelyancda@masterscada.ru>
2024-12-29 22:01:31 +0300

    add stats and some refactoring

--- VideoTelegramBot/TelegramBot.cs
+++ VideoTelegramBot/TelegramBot.cs
@@ -1,9 +1,7 @@
-﻿using AngleSharp.Dom;
-using Telegram.Bot;
+﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using YoutubeDownloader.Core.Downloading;
-using YoutubeExplode.Videos;
 using YoutubeExplode.Videos.Streams;
 
 namespace VideoTelegramBot;
@@ -11,6 +9,9 @@ namespace VideoTelegramBot;
 public class TelegramBot
 {
     private const char _argsSpliter = ' ';
+    private List<string> users = new();
+    private volatile int totoalVideoCount = 0;
+    private volatile int downloadTasksCount = 0;
     private CancellationTokenSource DownloadingCancellationTokenSource { get; set; } = new();
     private Container[] Containers { get; } = 
         [
@@ -59,36 +60,46 @@ public class TelegramBot
                 {
                     offset = update.Id + 1;
 
-                    if (update is null || update.Message is null)
+                    if (update is null || 
+                        update.Message is null || 
+                        update.Message.Type != MessageType.Text ||
+                        string.IsNullOrEmpty(update.Message.Chat.Username) || 
+                        string.IsNullOrEmpty(update.Message.Text))
                         continue;
 
-                    if(update.Message.Text == "/stop")
+                    if (!users.Contains(update.Message.Chat.Username))
+                        users.Add(update.Message.Chat.Username);
+
+                    switch (update.Message.Text)
                     {
-                        DownloadingCancellationTokenSource.Cancel();
-                        DownloadingCancellationTokenSource.Dispose();
-                        DownloadingCancellationTokenSource = new CancellationTokenSource();
-                        Console.WriteLine("all downloading stopped");
-                        continue;
+                        case "/start":
+                            continue;
+                        case "/stop":
+                            DownloadingCancellationTokenSource.Cancel();
+                            DownloadingCancellationTokenSource.Dispose();
+                            DownloadingCancellationTokenSource = new CancellationTokenSource();
+                            Console.WriteLine("all downloading stopped");
+                            break;
+                        case "/stats":
+                            telegramBot.SendMessage(update.Message.Chat, $"{totoalVideoCount} videos has been downloaded\nCurrent tasks count - {downloadTasksCount}\nMy users:\n{string.Join("\n", users)}", replyParameters: new ReplyParameters() { MessageId = update.Message.Id });
+                            break;
+                        default:
+                            var args = update.Message.Text.Split(_argsSpliter);
+
+                            foreach (var arg in args)
+                                if (Uri.TryCreate(arg, UriKind.Absolute, out var uri))
+                                    DownloadVideoAndSendLink(
+                                        telegramBot,
+                                        update.Message.Chat.Id,
+                                        update.Message.MessageId,
+                                        uri,
+                                        baseUrl,
+                                        baseSecureUrl,
+                                        args.Where(a => a != arg),
+                                        DownloadingCancellationTokenSource.Token);
+                            break;
+
                     }
-
-                    if (update.Message.Type != MessageType.Text ||
-                        string.IsNullOrEmpty(update.Message.Text) ||
-                        update.Message.Text == "/start")
-                        continue;
-
-                    var args = update.Message.Text.Split(_argsSpliter);
-
-                    foreach (var arg in args)
-                        if (Uri.TryCreate(arg, UriKind.Absolute, out var uri))
-                            DownloadVideoAndSendLink(
-                                telegramBot,
-                                update.Message.Chat.Id,
-                                update.Message.MessageId,
-                                uri,
-                                baseUrl,
-                                baseSecureUrl,
-                                args.Where(a => a != arg),
-                                DownloadingCancellationTokenSource.Token);
                 }
             }
         }
@@ -145,6 +156,8 @@ public class TelegramBot
             var replyMessageText = string.Empty;
             var index = startPlaylistIndex;
 
+            Interlocked.Increment(ref downloadTasksCount);
+
             foreach (var video in videos.Where(v => videos.IndexOf(v) >= startPlaylistIndex && videos.IndexOf(v) <= endPlaylistIndex))
             {
                 var videoIndex = (startPlaylistIndex == endPlaylistIndex || videos.Count == 1 ? string.Empty : $"[{index++}] ");
@@ -164,6 +177,7 @@ public class TelegramBot
                         replyMessage.Id,
                         replyMessageText, 
                         ParseMode.Html);
+                    Interlocked.Increment(ref totoalVideoCount);
                 }
                 catch (Exception ex) 
                 {
@@ -180,6 +194,10 @@ public class TelegramBot
         catch (Exception ex)
         {
             Console.WriteLine(ex.ToString());
+        }
+        finally
+        {
+            Interlocked.Decrement(ref downloadTasksCount);
         }
     }
 }
